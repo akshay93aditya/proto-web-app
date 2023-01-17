57339d2ca7c9a4fe8c848b23fa9b5b508a252015
abhishek <abbhishekk27@gmail.com>
2023-01-17 11:34:30 +0530

    add: checkin counter

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,7 @@
 import {
+	Center,
 	Circle,
+	Flex,
 	Heading,
 	Modal,
 	ModalBody,
@@ -10,9 +12,11 @@ import {
 	useDisclosure,
 } from '@chakra-ui/react';
 import { useWallet } from '@solana/wallet-adapter-react';
+import axios from 'axios';
 import Link from 'next/link';
 import React, { useState, useEffect, Suspense } from 'react';
 import Loading from '../components/Loading';
+import { FailedCheckInIcon, SuccessCheckInIcon } from '../dynamic/CheckInIcons';
 
 interface Region {
 	continents: number;
@@ -29,9 +33,30 @@ interface Places {
 export default function Options() {
 	const [regions, setRegions] = useState<Region | []>([]);
 	const [places, setPlaces] = useState<Places | []>([]);
-	const { connected, wallet } = useWallet();
+	const { connected, publicKey } = useWallet();
+	const [checkInCount, setCheckInCount] = useState();
 
 	const { isOpen, onOpen, onClose } = useDisclosure();
+
+	useEffect(() => {
+		async function getTimelineData() {
+			try {
+				const timelineResponse = await axios.get(
+					'https://proto-api.onrender.com/checkins',
+					{
+						params: { user_wallet_address: publicKey },
+					}
+				);
+				setCheckInCount(timelineResponse.data.length);
+				console.log(checkInCount);
+				console.log(timelineResponse.data.length);
+			} catch (err) {
+				console.log(err);
+			}
+		}
+
+		if (publicKey) getTimelineData();
+	}, [publicKey]);
 
 	useEffect(() => {
 		const timer = setTimeout(() => {
@@ -127,11 +152,32 @@ export default function Options() {
 							</Heading>
 						</Link>
 						<div className='w-1/2 relative bg-[#DEAD2A] rounded-lg m-2'>
+							{checkInCount && (
+								<Center>
+									<div className='flex mt-2'>
+										<div className='flex flex-col items-center'>
+											<Heading fontSize='42px' color='#fff'>
+												0
+											</Heading>
+											<SuccessCheckInIcon />
+										</div>
+										<Heading fontSize='42px' color='#fff' mx='8px'>
+											/
+										</Heading>
+										<div className='flex flex-col items-center'>
+											<Heading fontSize='42px' color='#fff'>
+												{checkInCount}
+											</Heading>
+											<FailedCheckInIcon />
+										</div>
+									</div>
+								</Center>
+							)}
 							<Heading
 								fontSize='2xl'
 								color='#fff'
 								className='absolute left-4 bottom-2'>
-								Check-In's
+								Check-Ins
 							</Heading>
 						</div>
 					</div>
