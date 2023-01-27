36b20b170cdcb5aeb3db9eccb0d9a1499964264a
Shreyash-Addepalli <shreyash.addepalli@gmail.com>
2023-01-28 00:23:43 +0530

    Checkin Coordinates fixed and history page added

---
+++ pages/history.tsx
@@ -0,0 +1,10 @@
+import React from 'react';
+import HistoricalCheckIns from '../components/HistoricalCheckIns';
+
+export default function History() {
+  return (
+    <>
+      <HistoricalCheckIns />
+    </>
+  );
+}
