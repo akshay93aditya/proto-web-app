183e5f594f7d9fcdd861fa826ea1c6d31af34a84
abhishek <abbhishekk27@gmail.com>
2023-01-27 18:24:27 +0530

    add: historical checkins page

--- components/HistoricalCheckIns.tsx
+++ components/HistoricalCheckIns.tsx
@@ -57,6 +57,7 @@ export default function HistoricalCheckIns() {
 
   useEffect(() => {
     fetchCheckins();
+    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [wallet]);
 
   useEffect(() => {
