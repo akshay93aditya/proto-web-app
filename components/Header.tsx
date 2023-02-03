2528e7f669842d6a588256d6293be3f8f71e950a
abhishek <abbhishekk27@gmail.com>
2023-02-03 14:10:56 +0530

    fix: toggle button

--- components/Header.tsx
+++ components/Header.tsx
@@ -16,29 +16,31 @@ export default function Header() {
   return (
     <div className="sticky top-0 z-20 flex w-full items-center justify-between bg-white py-2 px-2 shadow md:px-8">
       <LogoSmall onClick={() => router.push('/')} />
-      {router.pathname === '/' && (
-        <ChakraButton
-          colorScheme="telegram"
-          variant="outline"
-          color="primary"
-          mr="8px"
-          onClick={() => router.push('/history')}
-        >
-          View Checkin History
-        </ChakraButton>
-      )}
-      {router.pathname === '/history' && (
-        <ChakraButton
-          colorScheme="telegram"
-          variant="outline"
-          color="primary"
-          mr="8px"
-          onClick={() => router.push('/')}
-        >
-          New Check In
-        </ChakraButton>
-      )}
-      <Button />
+      <div className="ml-4 flex items-center">
+        {router.pathname === '/' && (
+          <ChakraButton
+            colorScheme="telegram"
+            variant="outline"
+            color="primary"
+            mr="8px"
+            onClick={() => router.push('/history')}
+          >
+            View Check-In History
+          </ChakraButton>
+        )}
+        {router.pathname === '/history' && (
+          <ChakraButton
+            colorScheme="telegram"
+            variant="outline"
+            color="primary"
+            mr="8px"
+            onClick={() => router.push('/')}
+          >
+            New Check-In
+          </ChakraButton>
+        )}
+        <Button />
+      </div>
     </div>
   );
 }
